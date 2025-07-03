15d974480c4af2d62f6f7abea336ebb2b93d08c2
Krishnaghartimagar <unknown420magar@gmail.com>
2025-07-03 11:36:42 +0545

    odd and even chaking

---
+++ OneDrive/Desktop/Main/java/Main.java
@@ -0,0 +1,20 @@
+//check whether the given number is even or odd
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter hte number:");
+        int n=input.nextInt();
+         if(n<0){
+             System.out.println("Enter posative number");
+             
+         }
+         if(n%2==0)
+         {
+             System.out.println(n+"is even");
+         }else{
+             System.out.println(n+"is odd");
+         }
+    }
+}
